b6205db122690217b2c723498afcb6241ab06a3e
krispin <krispinblow@gmail.com>
2022-02-27 11:07:55 -0600

    chapter 1 domain modeling branch

--- model.py
+++ model.py
@@ -145,5 +145,3 @@ class Batch:
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty 
     
-    #I am typing to create the BRANCH
-    
